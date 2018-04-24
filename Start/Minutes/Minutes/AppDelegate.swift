aada10e8d098da5fde3b3322cee8a7d61da19afd
mytest11111 <mytest11111@outlook.com>
2018-04-24 14:31:17 -0700

    Update

--- Start/Minutes/Minutes/AppDelegate.swift
+++ Start/Minutes/Minutes/AppDelegate.swift
@@ -1,5 +1,13 @@
 import UIKit
 
+import AppCenter
+import AppCenterDistribute
+import AppCenterCrashes
+import AppCenterAnalytics
+import AppCenterPush
+
+// 870abd56-2ce0-466e-be0e-e0282cc96f9d
+
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate
 {
@@ -8,7 +16,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-
+        
+        MSAppCenter.start("be17a075-6ad8-44af-9bab-58037e555397", withServices: [MSDistribute.self, MSCrashes.self, MSAnalytics.self, MSPush.self])
+        
         return true
     }
 
